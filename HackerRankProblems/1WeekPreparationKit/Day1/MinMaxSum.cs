2235a1b9c397310136e0c71ed91f9f7048d08ef9
Abdul Moiz <moiza0799@gmail.com>
2024-06-02 23:23:47 -0500

    MinMaxSum

---
+++ HackerRankProblems/1WeekPreparationKit/Day1/MinMaxSum.cs
@@ -0,0 +1,34 @@
+﻿
+namespace HackerRankProblems._1WeekPreparationKit.Day1
+{
+    internal class MinMaxSum
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            MinMaxSumResult.miniMaxSum(arr);
+        }
+    }
+    public class MinMaxSumResult
+    {
+
+        public static void miniMaxSum(List<int> arr)
+        {
+
+
+            long min = 0, max = 0, sum = 0;
+            arr.Sort();
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            min = sum - arr[arr.Count - 1];
+            max = sum - arr[0];
+            Console.Write(min.ToString() + " " + max.ToString());
+
+        }
+
+    }
+}
