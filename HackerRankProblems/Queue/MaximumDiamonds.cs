d5ddf017d8ef67d0c201b7a3cf58e4661d57884b
Abdul Moiz <moiza0799@gmail.com>
2024-09-10 09:04:29 -0500

    Solved via priority queue;

--- HackerRankProblems/Queue/MaximumDiamonds.cs
+++ HackerRankProblems/Queue/MaximumDiamonds.cs
@@ -4,7 +4,7 @@ namespace MaximumDiamonds
     {
         // static void Main(String[] args)
         // {
-        //     int[] arr = {7, 1, 2};
+        //     int[] arr = { 7, 1, 2 };
         //     long result = Solution.maxDiamonds(arr, arr.Length, 2);
         //     System.Console.WriteLine(result);
         // }
@@ -13,13 +13,20 @@ namespace MaximumDiamonds
     {
         public static long maxDiamonds(int[] A, int N, int K)
         {
+            var pQ = new PriorityQueue<int, int>();
             long sum = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                pQ.Enqueue(A[i], -1 * A[i]);
+            }
 
             for (int i = 0; i < K; i++)
             {
-                Array.Sort(A);
-                sum += A[N - 1];
-                A[N - 1] = A[N - 1] / 2;
+                int value = pQ.Dequeue();
+                sum += value;
+                value = value / 2;
+                pQ.Enqueue(value, -1 * value);
             }
             return sum;
         }
