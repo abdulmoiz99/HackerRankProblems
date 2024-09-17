5a036fd6247b791149d4246d0498da4c4577d1b3
Abdul Moiz <moiza0799@gmail.com>
2024-09-16 20:34:07 -0500

    TicketCounter

---
+++ HackerRankProblems/Queue/TicketCounter.cs
@@ -0,0 +1,15 @@
+class TicketCounter
+{
+    public List<string> generate(int N)
+    {
+        List<string> binaryNumbers = new List<string>();
+        if (N <= 0) return binaryNumbers;
+
+        for (int i = 1; i <= N; i++)
+        {
+            string number = Convert.ToString(i, 2);
+            binaryNumbers.Add(number);
+        }
+        return binaryNumbers;
+    }
+}
