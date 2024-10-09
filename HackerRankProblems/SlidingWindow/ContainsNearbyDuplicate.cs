652171c962bf4d6ce2179d0cdcc85f544dda7387
Abdul Moiz <moiza0799@gmail.com>
2024-10-09 13:46:10 -0500

    ContainsNearbyDuplicate

---
+++ HackerRankProblems/SlidingWindow/ContainsNearbyDuplicate.cs
@@ -0,0 +1,15 @@
+public class ContainsNearbyDuplicate {
+    public bool ContainsNearbyDuplicate1(int[] nums, int k) {
+        HashSet<int> window = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(window.Contains(nums[i])) return true;
+
+            window.Add(nums[i]);
+
+            if(window.Count > k){
+                window.Remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
