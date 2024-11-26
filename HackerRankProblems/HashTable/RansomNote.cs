ed79ec559cfacc5e913905d554f6658072bdd779
Abdul Moiz <moiza0799@gmail.com>
2024-11-26 10:58:50 -0600

    FirstUniqChar

--- HackerRankProblems/HashTable/RansomNote.cs
+++ HackerRankProblems/HashTable/RansomNote.cs
@@ -4,12 +4,12 @@ namespace HackerRankProblems.HashTable
 {
     internal class RansomNote
     {
-        public static void Main(string[] args)
-        {
-            string ransomNote = "bg";
-            string magazine = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
-            Console.WriteLine(CanConstruct(ransomNote, magazine));
-        }
+        //public static void Main(string[] args)
+        //{
+        //    string ransomNote = "bg";
+        //    string magazine = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+        //    Console.WriteLine(CanConstruct(ransomNote, magazine));
+        //}
         public static bool CanConstruct(string ransomNote, string magazine)
         {
             int[] charCounts = new int[26];
