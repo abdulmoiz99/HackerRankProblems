a26e75909dd5f8507ad7c38fbf939cbde114d117
Abdul Moiz <moiza0799@gmail.com>
2024-11-26 08:07:36 -0600

    Solved Ransom Notes

---
+++ HackerRankProblems/HashTable/RansomNote.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace HackerRankProblems.HashTable
+{
+    internal class RansomNote
+    {
+        public static void Main(string[] args)
+        {
+            string ransomNote = "bg";
+            string magazine = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+            Console.WriteLine(CanConstruct(ransomNote, magazine));
+        }
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            int[] charCounts = new int[26];
+            foreach (char item in magazine)
+            {
+                charCounts[item - 'a']++;
+            }
+            foreach (char item in ransomNote)
+            {
+                if (charCounts[item - 'a'] > 0)
+                {
+                    charCounts[item - 'a']--;
+                }
+                else return false;
+            }
+            return true;
+        }
+    }
+}
