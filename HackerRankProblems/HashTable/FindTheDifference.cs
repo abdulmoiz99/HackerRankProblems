72cce9b2ebcfa4ed2587a5d762219a9c0303b845
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 07:56:05 -0600

    Code changes

--- HackerRankProblems/HashTable/FindTheDifference.cs
+++ HackerRankProblems/HashTable/FindTheDifference.cs
@@ -1,33 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HackerRankProblems.HashTable
+﻿namespace HackerRankProblems.HashTable
 {
     internal class FindTheDifference
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine(FindTheDifference1("a", "aa"));
-        }
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine(FindTheDifference1("ae", "aea"));
+        //}
         public static char FindTheDifference1(string s, string t)
         {
-            var dictionary = new Dictionary<char, int>();
-            foreach (var item in s)
+            char result = '\0';
+
+            foreach (char c in s)
             {
-                if (dictionary.ContainsKey(item))
-                    dictionary[item]++;
-                else dictionary.Add(item, 1);
+                result ^= c;
             }
-            foreach (var item in t)
+            foreach (char c in t)
             {
-                if (dictionary.ContainsKey(item) && dictionary[item] > 0)
-                    dictionary[item]--;
-                else return item;
+                result ^= c;
             }
-            return ' ';
+            return result;
         }
 
     }
