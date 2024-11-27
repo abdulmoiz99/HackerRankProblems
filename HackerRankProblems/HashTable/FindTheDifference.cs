ac75f769eda6f73c0917c00e1cf6786dafc58a46
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 07:43:24 -0600

    Solved Find the difference

---
+++ HackerRankProblems/HashTable/FindTheDifference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems.HashTable
+{
+    internal class FindTheDifference
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheDifference1("a", "aa"));
+        }
+        public static char FindTheDifference1(string s, string t)
+        {
+            var dictionary = new Dictionary<char, int>();
+            foreach (var item in s)
+            {
+                if (dictionary.ContainsKey(item))
+                    dictionary[item]++;
+                else dictionary.Add(item, 1);
+            }
+            foreach (var item in t)
+            {
+                if (dictionary.ContainsKey(item) && dictionary[item] > 0)
+                    dictionary[item]--;
+                else return item;
+            }
+            return ' ';
+        }
+
+    }
+}
