ae9ba1999fc428f113042c62d59eb5dc26f2edfb
Abdul Moiz <moiza0799@gmail.com>
2024-11-28 15:19:01 -0600

    SetMismatch

---
+++ HackerRankProblems/HashTable/SetMismatch.cs
@@ -0,0 +1,35 @@
+﻿namespace HackerRankProblems.HashTable
+{
+    internal class SetMismatch
+    {
+        //public static void Main(string[] args)
+        //{
+        //    int[] nums = { 2, 2 };
+        //    Console.WriteLine(FindErrorNums(nums));
+        //}
+        public static int[] FindErrorNums(int[] nums)
+        {
+            return new int[] { FindDuplicatedValue(nums), FindMissingValues(nums) };
+        }
+        public static int FindMissingValues(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                    return i;
+            }
+            return -1;
+        }
+        public static int FindDuplicatedValue(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (set.Contains(item)) { return item; }
+                set.Add(item);
+            }
+            return -1;
+        }
+    }
+}
