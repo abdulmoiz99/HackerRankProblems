1a44c8f10e480d9ad8620d28fe0f8ea84b472d23
Abdul Moiz <moiza0799@gmail.com>
2024-12-02 17:21:19 -0600

    UniqueMorseRepresentations

---
+++ HackerRankProblems/HashTable/MorseCode.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRankProblems.HashTable
+{
+    internal class MorseCode
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            var set = new HashSet<string>();
+            foreach (var word in words)
+            {
+                set.Add(GetMorseCode(word));
+            }
+            return set.Count();
+        }
+        public string GetMorseCode(string word)
+        {
+            string[] code = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            var result = new StringBuilder();
+            foreach (char c in word)
+            {
+                int index = c - 'a';
+                result.Append(code[index]);
+            }
+            return result.ToString();
+        }
+    }
+}
