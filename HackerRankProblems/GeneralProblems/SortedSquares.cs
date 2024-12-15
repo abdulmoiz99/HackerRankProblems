7a0fd5651f1846cf82b77ed057a620beac1e4915
Abdul Moiz <moiza0799@gmail.com>
2024-12-15 14:49:29 -0600

    SortedSquares

---
+++ HackerRankProblems/GeneralProblems/SortedSquares.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems.GeneralProblems
+{
+    internal class SortedSquares
+    {
+    //    public static void Main(string[] args)
+    //    {
+    //        int[] array = { -4, -1, 0, 3, 10 };
+
+    //        int i = 0, j = array.Length - 1;
+
+    //        while (j >= 0)
+    //        {
+    //            if (Math.Abs(array[i]) > Math.Abs(array[j]))
+    //            {
+    //                (array[i], array[j]) = (array[j], array[i]);
+    //            }
+    //            j--;
+    //        }
+    //        Console.WriteLine();
+
+    //    }
+    }
+}
