9df7dca795b744c2bad691ae0119aaeeaedaefdf
Abdul Moiz <moiza0799@gmail.com>
2025-01-08 20:37:19 -0600

    PrefixCount

---
+++ HackerRankProblems/GeneralProblems/CountingWords.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class CountingWords
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int count = 0;
+            int length = pref.Length;
+            foreach (var item in words)
+            {
+                if (item.Length >= length && item.StartsWith(pref))
+                {
+                    count++;
+                }
+                *
+            }
+            return count;
+        }
+    }
+}
