4f314aa5f484386895f8939877f2811ac0bebdcc
Abdul Moiz <moiza0799@gmail.com>
2025-01-13 15:05:37 -0600

    MinLengthAfterOperatios

--- HackerRankProblems/GeneralProblems/CountingWords.cs
+++ HackerRankProblems/GeneralProblems/CountingWords.cs
@@ -12,7 +12,6 @@
                 {
                     count++;
                 }
-                *
             }
             return count;
         }
---
+++ HackerRankProblems/GeneralProblems/MinLengthAfterOperatios.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class MinLengthAfterOperatios
+    {
+        public int MinimumLength(string s)
+        {
+            var dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dictionary.ContainsKey(s[i]))
+                    dictionary[s[i]]++;
+                else dictionary.Add(s[i], 1);
+            }
+            int result = 0;
+            foreach (var item in dictionary)
+            {
+                if (item.Value >= 3)
+                {
+                    int value = item.Value;
+                    while (value >= 3)
+                        value = value - 2;
+                    result += value;
+                }
+                else result += item.Value;
+            }
+            return result;
+        }
+    }
+}
