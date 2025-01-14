591ffb52f1d045ea046aeb4c61b2a66e36e73e10
Abdul Moiz <moiza0799@gmail.com>
2025-01-14 13:37:52 -0600

    FindThePrefixCommonArray

---
+++ HackerRankProblems/GeneralProblems/FindThePrefixCommonArraySolution.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class FindThePrefixCommonArraySolution
+    {
+        public int[] FindThePrefixCommonArray(int[] A, int[] B)
+        {
+            int[] freq = new int[A.Length];
+            int[] result = new int[A.Length];
+
+            int ans = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                int a = A[i] - 1, b = B[i] - 1;
+                freq[a]++;
+                freq[b]++;
+
+                if (freq[a] == 2 && a != b) ans++;
+                if (freq[b] == 2) ans++;
+
+                result[i] = ans;
+            }
+
+            return result;
+        }
+    }
+}
