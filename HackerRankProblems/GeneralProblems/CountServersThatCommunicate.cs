beb4d6eeab8ba59a0111625201956927c3568828
Abdul Moiz <moiza0799@gmail.com>
2025-01-23 15:25:27 -0600

    CountServersThatCommunicate

---
+++ HackerRankProblems/GeneralProblems/CountServersThatCommunicate.cs
@@ -0,0 +1,33 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class CountServersThatCommunicate
+    {
+        public int CountServers(int[][] grid)
+        {
+            var rows = new int[grid[0].Length];
+            var cols = new int[grid.Length];
+            var list = new List<(int, int)>();
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        list.Add((i, j));
+                        cols[i]++;
+                        rows[j]++;
+
+                    }
+                }
+            }
+            int count = 0;
+            foreach (var value in list)
+            {
+                if (rows[value.Item2] > 1 || cols[value.Item1] > 1)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
