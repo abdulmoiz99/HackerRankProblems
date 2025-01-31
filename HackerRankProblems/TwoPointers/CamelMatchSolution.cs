21a90b258e87fa292f1ef0a2cd6a5208747580ac
Abdul Moiz <moiza0799@gmail.com>
2025-01-31 17:10:42 -0600

    CamelMatchSolution

---
+++ HackerRankProblems/TwoPointers/CamelMatchSolution.cs
@@ -0,0 +1,31 @@
+﻿namespace HackerRankProblems.TwoPointers
+{
+    internal class CamelMatchSolution
+    {
+        //public static void Main(string[] args)
+        //{
+        //    var query = "ControlPanel";
+        //    var pattern = "CooP";
+        //    Console.WriteLine(IsMatch(query, pattern));
+        }
+        public static bool IsMatch(string query, string pattern)
+        {
+            int i = 0, j = 0;
+            for (i = 0; i < query.Length; i++)
+            {
+                if (char.IsUpper(query[i]))
+                {
+                    if (j >= pattern.Length || query[i] != pattern[j])
+                        return false;
+                    j++;
+                }
+                else if (j < pattern.Length && query[i] == pattern[j])
+                {
+                    j++;
+                }
+            }
+
+            return j == pattern.Length;
+        }
+    }
+}
