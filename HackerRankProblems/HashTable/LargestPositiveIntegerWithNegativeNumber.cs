2a87190991550281aa7108ebcce48d023804a537
Abdul Moiz <moiza0799@gmail.com>
2025-02-01 11:12:20 -0600

    LargestPositiveIntegerWithNegativeNumber

---
+++ HackerRankProblems/HashTable/LargestPositiveIntegerWithNegativeNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRankProblems.HashTable
+{
+    internal class LargestPositiveIntegerWithNegativeNumber
+    {
+        public int FindMaxK(int[] nums)
+        {
+            var set = new HashSet<int>();
+            int max = -1;
+            foreach (var num in nums)
+            {
+                int value = Math.Abs(num);
+                if ((num > 0 && set.Contains(num * -1)) || (num < 0 && set.Contains(value)))
+                {
+                    if (value > max) max = value;
+                }
+                else set.Add(num);
+            }
+            return max;
+        }
+    }
+}
