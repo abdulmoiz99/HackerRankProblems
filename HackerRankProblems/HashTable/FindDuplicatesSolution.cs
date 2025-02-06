e1cb52e9507f32bc600ca648b1c98cd897274a22
Abdul Moiz <moiza0799@gmail.com>
2025-02-06 13:40:44 -0600

    Find Duplicate

---
+++ HackerRankProblems/HashTable/FindDuplicatesSolution.cs
@@ -0,0 +1,17 @@
+﻿namespace HackerRankProblems.HashTable
+{
+    internal class FindDuplicatesSolution
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (set.Contains(item)) return item;
+                else set.Add(item);
+            }
+
+            return -1;
+        }
+    }
+}
