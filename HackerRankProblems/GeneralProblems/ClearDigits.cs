6fa5c6d59d0efbb39322be802e352052b0b0648a
Abdul Moiz <moiza0799@gmail.com>
2025-02-10 13:55:24 -0600

    ClearDigits

---
+++ HackerRankProblems/GeneralProblems/ClearDigits.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace HackerRankProblems.GeneralProblems
+{
+    internal class ClearDigits
+    {
+        public string ClearDigitsSol(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                }
+                else sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
