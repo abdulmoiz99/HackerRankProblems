57e99243756de7b628bb557bf8e891c396ea710e
Abdul Moiz <moiza0799@gmail.com>
2025-02-11 15:45:38 -0600

    SortArrayByParitySol

---
+++ HackerRankProblems/TwoPointers/SortArrayByParitySol.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRankProblems.TwoPointers
+{
+    internal class SortArrayByParitySol
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            var result = new int[nums.Length];
+            int index = 0;
+            foreach (var item in nums)
+            {
+                if (item % 2 == 0)
+                    result[index++] = item;
+            }
+
+            foreach (var item in nums)
+            {
+                if (item % 2 != 0)
+                    result[index++] = item;
+            }
+            return result;
+        }
+    }
+}
