2b88fdda8404700205fc66425e7300d8a2995203
Abdul Moiz <moiza0799@gmail.com>
2025-02-11 16:07:32 -0600

    DistinctAveragesSol

---
+++ HackerRankProblems/TwoPointers/DistinctAveragesSol.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRankProblems.TwoPointers
+{
+    internal class DistinctAveragesSol
+    {
+        public int DistinctAverages(int[] nums)
+        {
+            var set = new HashSet<double>();
+            System.Array.Sort(nums);
+            int start = 0, end = nums.Length - 1;
+            while (start < end)
+            {
+                double average = nums[start++] + nums[end--];
+                set.Add(average);
+            }
+
+            return set.Count();
+        }
+    }
+}
