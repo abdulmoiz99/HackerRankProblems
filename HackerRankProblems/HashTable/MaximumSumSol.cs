1166a47b994abf9c1350fa3d2d9707d5eb222400
Abdul Moiz <moiza0799@gmail.com>
2025-02-12 13:29:51 -0600

    MaximumSum

---
+++ HackerRankProblems/HashTable/MaximumSumSol.cs
@@ -0,0 +1,40 @@
+﻿namespace HackerRankProblems.HashTable
+{
+    internal class MaximumSumSol
+    {
+        public int MaximumSum(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+            int max = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var value = GetSum(nums[i]);
+                if (dictionary.ContainsKey(value))
+                {
+                    int sum = nums[i] + nums[dictionary[value]];
+                    int index = dictionary[value];
+                    if (sum > max)
+                        max = sum;
+
+                    if (nums[i] > nums[dictionary[value]])
+                    {
+                        dictionary[value] = i;
+                    }
+                }
+                else dictionary.Add(value, i);
+            }
+
+            return max;
+        }
+        public int GetSum(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num = num / 10;
+            }
+            return sum;
+        }
+    }
+}
