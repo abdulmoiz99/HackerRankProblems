4980991e807d480bdf20f721cfd2d7781af90647
Abdul Moiz <moiza0799@gmail.com>
2025-02-13 14:11:45 -0600

    MinOperationToReachThreshold

---
+++ HackerRankProblems/GeneralProblems/MinOperationToReachThreshold.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class MinOperationToReachThreshold
+    {
+        public int MinOperations(int[] nums, int k)
+        {
+            var pq = new PriorityQueue<long, long>();
+            int count = 0;
+            foreach (var item in nums)
+            {
+                pq.Enqueue(item, item);
+            }
+            while (pq.Count > 1 && pq.Peek() < k)
+            {
+                long value = (pq.Dequeue() * 2) + pq.Dequeue();
+                pq.Enqueue(value, value);
+                count++;
+            }
+            return count;
+        }
+    }
+}
