936d6daff7073da35d68513d7f4180dacfd3d4c1
Abdul Moiz <moiza0799@gmail.com>
2025-02-14 14:03:06 -0600

    ProductOfNumbers

---
+++ HackerRankProblems/GeneralProblems/ProductOfNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRankProblems.GeneralProblems
+{
+    internal class ProductOfNumbers
+    {
+        List<int> values;
+        public ProductOfNumbers()
+        {
+            values = new List<int>();
+        }
+
+        public void Add(int num)
+        {
+            values.Add(num); ;
+        }
+
+        public int GetProduct(int k)
+        {
+            var startIndex = (values.Count - k);
+            int product = 1;
+            for (int i = startIndex; i < values.Count; i++)
+            {
+                if (values[i] == 0) return 0;
+                product *= values[i];
+            }
+            return product;
+        }
+    }
+}
