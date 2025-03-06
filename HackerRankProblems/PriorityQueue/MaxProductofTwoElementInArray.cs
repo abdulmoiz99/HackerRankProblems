5bcbe729b23eeb1c00a96cddafd5bebf55e9a9f1
Abdul Moiz <moiza0799@gmail.com>
2025-03-05 20:46:13 -0600

    MaxProductofTwoElementInArray

---
+++ HackerRankProblems/PriorityQueue/MaxProductofTwoElementInArray.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRankProblems.PriorityQueue
+{
+    class MaxProductofTwoElementInArray
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int firstMax = 0, secondMax = 0;
+            foreach (var num in nums)
+            {
+                if (num > firstMax)
+                {
+                    secondMax = firstMax;
+                    firstMax = num;
+                }
+                else if (num > secondMax)
+                {
+                    secondMax = num;
+                }
+            }
+            return (firstMax - 1) * (secondMax - 1);
+        }
+    }
+}
